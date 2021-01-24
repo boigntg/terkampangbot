435b2e51ea00527544ab2155eb7262d5a6ef6576
otakudesuShoko <76880423+otakudesuShoko@users.noreply.github.com>
2021-01-24 19:57:38 +0800

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -22,7 +22,7 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    update.effective_message.reply_text("{} is now AFK!".format(update.effective_user.first_name))
+    update.effective_message.reply_text("{} sekarang AFK!".format(update.effective_user.first_name))
 
 
 @run_async
@@ -34,7 +34,7 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        update.effective_message.reply_text("{} is no longer AFK!".format(update.effective_user.first_name))
+        update.effective_message.reply_text("{} sudah tidak AFK!!".format(update.effective_user.first_name))
 
 
 @run_async
@@ -61,9 +61,9 @@ def reply_afk(bot: Bot, update: Update):
             if sql.is_afk(user_id):
                 user = sql.check_afk_status(user_id)
                 if not user.reason:
-                    res = "{} is AFK!".format(fst_name)
+                    res = "{} sedang AFK!!".format(fst_name)
                 else:
-                    res = "{} is AFK! says its because of:\n{}".format(fst_name, user.reason)
+                    res = "{} sedang AFK!\nKarena :{}".format(fst_name, user.reason)
                 message.reply_text(res)
 
 
@@ -72,10 +72,10 @@ def __gdpr__(user_id):
 
 
 __help__ = """
- - /afk <reason>: mark yourself as AFK.
- - brb <reason>: same as the afk command - but not a command.
+ - /afk <alasan>: tandai dirimu sebagai AFK.
+ - brb <alasan>: sama dengan perintah afk - tetapi bukan perintah.
 
-When marked as AFK, any mentions will be replied to with a message to say you're not available!
+Ketika ditandai sebagai AFK, sebutan apa pun akan dibalas dengan pesan untuk mengatakan Anda tidak tersedia!
 """
 
 __mod_name__ = "AFK"
